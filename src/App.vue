30310cc114d71a76230e8a9c66aea9cf7914af2f
yasinitskyi <yuralee007@gmail.com>
2021-01-27 12:34:53 +0800

    Created: basic validation

--- src/App.vue
+++ src/App.vue
@@ -1,22 +1,73 @@
 <template>
-  <div>
-    <app-counter></app-counter>
-    <app-car></app-car>
-    <app-onoff v-model="switched"></app-onoff>
+  <div class="container">
+    <form class="pt-3">
+      <div class="form-group">
+        <label for="email">Email</label>
+        <input type="email" id="email" class="form-control" v-model="email" @blur="$v.email.$touch()"
+          :class="{'is-invalid': $v.email.$error}">
+        <div class="invalid-feedback" v-if="!$v.email.required">
+          It is required filed
+        </div>
+        <div class="invalid-feedback" v-if="!$v.email.email">
+          This should be an email
+        </div>
+      </div>
+
+      <div class="form-group">
+        <label for="password">Password</label>
+        <input type="password" id="password" class="form-control" v-model="password" @blur="$v.password.$touch()"
+          :class="{'is-invalid': $v.password.$error}">
+        <div class="invalid-feedback" v-if="!$v.password.minLength">
+          Min {{$v.password.$params.minLength.min}}. Now it is {{password.length}}
+        </div>
+        <div class="invalid-feedback" v-if="!$v.password.required">
+          It is required filed
+        </div>
+      </div>
+
+      <div class="form-group">
+        <label for="confirmPassword">Confrim</label>
+        <input type="password" id="confirmPassword" class="form-control" v-model="confirmPassword"
+          @blur="$v.confirmPassword.$touch()" :class="{'is-invalid': $v.confirmPassword.$error}">
+        <div class="invalid-feedback" v-if="!$v.confirmPassword.sameAs">
+          Passwords do not match
+        </div>
+      </div>
+
+    </form>
   </div>
 </template>
 
 <script>
+import { required, email, minLength, sameAs } from 'vuelidate/lib/validators'
 export default {
   name: 'app',
   data() {
     return {
-      switched: false,
+      email: '',
+      password: '',
+      confirmPassword: ''
+    }
+  },
+  validations: {
+    email: {
+      required: required,
+      email: email
+    },
+    password: {
+      minLength: minLength(6),
+      required: required
+    },
+    confirmPassword: {
+      sameAs: sameAs('password')
     }
   }
 }
 </script>
 
-<style lang="sass">
-
+<style lang="sass" scoped>
+  .invalid-feedback
+    display: block
+  .form-group
+    margin-bottom: 20px
 </style>
--- src/main.js
+++ src/main.js
@@ -1,3 +1,4 @@
+import Vuelidate from 'vuelidate';
 import Vue from 'vue';
 import App from './App.vue';
 import Car from './Car.vue';
@@ -5,6 +6,8 @@ import Counter from './Counter.vue';
 import Color from './color.js';
 import Onoff from './Onoff.vue';
 export const eventEmitter = new Vue();
+
+Vue.use(Vuelidate);
 
 Vue.component('app-car', Car);
 Vue.component('app-counter', Counter);
