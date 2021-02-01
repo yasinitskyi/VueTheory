d5bb16b1abfcae1b65ae4b8742c02d7a7cc9158d
yasinitskyi <yuralee007@gmail.com>
2021-02-01 13:35:22 +0800

    Pushed: all initial value

--- src/main.js
+++ src/main.js
@@ -7,10 +7,12 @@ import Counter from './Counter.vue';
 import Color from './color.js';
 import Onoff from './Onoff.vue';
 import router from './routes.js';
+import VueResource from 'vue-resource';
 export const eventEmitter = new Vue();
 
 Vue.use(Vuelidate);
 Vue.use(VueRouter);
+Vue.use(VueResource);
 
 Vue.component('app-car', Car);
 Vue.component('app-counter', Counter);
